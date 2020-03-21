9c15ba966cdb72ceb51a7d5f29a84afdd99099b3
Arkanium77 <arkanium77@gmail.com>
2020-03-21 16:57:35 +0300

    Split main PersonService code into several methods.

--- src/main/java/ru/liga/intership/badcode/service/PersonService.java
+++ src/main/java/ru/liga/intership/badcode/service/PersonService.java
@@ -3,10 +3,7 @@ package ru.liga.intership.badcode.service;
 
 import ru.liga.intership.badcode.domain.Person;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.ResultSet;
-import java.sql.Statement;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,42 +11,53 @@ public class PersonService {
 
 
     /**
-     * Возвращает средний индекс массы тела всех лиц мужского пола старше 18 лет
-     *
-     * @return
+     * Выводит средний индекс массы тела всех лиц мужского пола старше 18 лет
      */
     public void getAdultMaleUsersAverageBMI() {
         double totalImt = 0.0;
         long countOfPerson = 0;
         try {
-
-            Connection conn = DriverManager.getConnection("jdbc:hsqldb:mem:test", "sa", "");
-            Statement statement = conn.createStatement();
-            ResultSet rs = statement.executeQuery("SELECT * FROM person WHERE sex = 'male' AND age > 18");
-            List<Person> adultPersons = new ArrayList<>();
-            while (rs.next()) {
-                Person p = new Person();
-                //Retrieve by column name
-                p.setId(rs.getLong("id"));
-                p.setSex(rs.getString("sex"));
-                p.setName(rs.getString("name"));
-                p.setAge(rs.getLong("age"));
-                p.setWeight(rs.getLong("weight"));
-                p.setHeight(rs.getLong("height"));
-                adultPersons.add(p);
-            }
-
-            for (Person p : adultPersons) {
-                double heightInMeters = p.getHeight() / 100d;
-                double imt = p.getWeight() / (Double) (heightInMeters * heightInMeters);
-                totalImt += imt;
-            }
+            List<Person> adultPersons = getAdultPersonsFromDb();
+            totalImt = getTotalImt(adultPersons);
             countOfPerson = adultPersons.size();
 
         } catch (Exception e) {
             e.printStackTrace();
         }
         System.out.println("Average imt - " + totalImt / countOfPerson);
+    }
+
+    private double getTotalImt(List<Person> adultPersons) {
+        double totalImt = 0.0;
+        for (Person p : adultPersons) {
+            double heightInMeters = p.getHeight() / 100d;
+            double imt = p.getWeight() / (Double) (heightInMeters * heightInMeters);
+            totalImt += imt;
+        }
+        return totalImt;
+    }
+
+    private List<Person> getAdultPersonsFromDb() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:hsqldb:mem:test", "sa", "");
+        Statement statement = conn.createStatement();
+        ResultSet rs = statement.executeQuery("SELECT * FROM person WHERE sex = 'male' AND age > 18");
+        return personsByResultSet(rs);
+    }
+
+    private List<Person> personsByResultSet(ResultSet rs) throws SQLException {
+        List<Person> adultPersons = new ArrayList<>();
+        while (rs.next()) {
+            Person p = new Person();
+            //Retrieve by column name
+            p.setId(rs.getLong("id"));
+            p.setSex(rs.getString("sex"));
+            p.setName(rs.getString("name"));
+            p.setAge(rs.getLong("age"));
+            p.setWeight(rs.getLong("weight"));
+            p.setHeight(rs.getLong("height"));
+            adultPersons.add(p);
+        }
+        return adultPersons;
     }
 
 }
