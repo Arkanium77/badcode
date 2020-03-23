79fe8344b53d86bac5330acdc7ff34e6a9e44144
Arkanium77 <arkanium77@gmail.com>
2020-03-23 16:12:07 +0300

    AUnworked test :(

---
+++ src/test/java/ru/liga/intership/badcode/service/PersonDatabaseExtractorTest.java
@@ -0,0 +1,34 @@
+package ru.liga.intership.badcode.service;
+
+//@RunWith(SpringRunner.class)
+//@SpringBootTest
+//@JdbcTest
+public class PersonDatabaseExtractorTest {
+    /*
+    static DatabaseExtractor extractor;
+
+    @BeforeClass
+    public static void beforeClass() {
+        extractor = new PersonDatabaseExtractor("jdbc:hsqldb:mem:test", "sa", "");
+    }
+
+    @Test
+    public void extractAllPersonsReturn5SizeList() {
+        String query = "SELECT * FROM person";
+        Assertions.assertThat(extractor.extractByQuery(query).size()).isEqualTo(5);
+    }
+
+    @Test
+    public void extractAdultPersonsReturn4SizeList() {
+        String query = "SELECT * FROM person WHERE age>=18";
+        Assertions.assertThat(extractor.extractByQuery(query).size()).isEqualTo(4);
+    }
+
+    @Test
+    public void extractNonAdultPersonsReturn1SizeList() {
+        String query = "SELECT * FROM person WHERE age<18";
+        Assertions.assertThat(extractor.extractByQuery(query).size()).isEqualTo(1);
+    }
+
+ */
+}
